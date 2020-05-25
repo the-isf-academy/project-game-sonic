030c10b3c90a6ac335468e692dadcd34cdc124c4
Maddalena <0010024@student.isf.edu.hk>
2020-05-25 11:39:12 +0800

    added new files

---
+++ island/dungeon deco.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="dungeon deco" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../../../Downloads/RFCastle_1.0/decorative.png" width="512" height="512"/>
+</tileset>
---
+++ island/dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="dungeon" tilewidth="32" tileheight="32" tilecount="414" columns="18">
+ <image source="../../../../../Downloads/RFCastle_1.0/mainlevbuild.png" width="592" height="736"/>
+</tileset>
