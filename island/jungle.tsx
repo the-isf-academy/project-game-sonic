c097d1af3fa50f595686b30e1def976171baf54f
Maddalena <0010024@student.isf.edu.hk>
2020-05-26 10:06:21 +0800

    added a tile set What I changed (Replace this with a description of what you changed in this commit. This should be 1-2 sentences.)
    Why I changed it
    (Describe why you made these changes. Were you working toward a goal? Did you reorganize your code? This should be 1-2 sentences.)
    
    Estimate for remaining time to finish assignment: [REPLACE WITH TIME ESTIMATE]

---
+++ island/jungle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="jungle" tilewidth="32" tileheight="32" tilecount="110" columns="11">
+ <image source="../../../../../Downloads/tf_jungle/tf_jungle_tileset.png" width="352" height="336"/>
+</tileset>
