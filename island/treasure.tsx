03f6cf63affea48d822af96a04741fa7f85b5745
Maddalena <0010024@student.isf.edu.hk>
2020-05-26 10:11:00 +0800

    new tile sets What I changed (Replace this with a description of what you changed in this commit. This should be 1-2 sentences.)
    Why I changed it
    (Describe why you made these changes. Were you working toward a goal? Did you reorganize your code? This should be 1-2 sentences.)
    
    Estimate for remaining time to finish assignment: [REPLACE WITH TIME ESTIMATE]

---
+++ island/treasure.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="treasure" tilewidth="32" tileheight="32" tilecount="324" columns="18">
+ <image source="../../../../../Downloads/73nunEG.png" width="600" height="600"/>
+</tileset>
