51504487955a816cfcccb3ff4997982e80442d7c
Jared <0010100@student.isf.edu.hk>
2020-05-28 18:16:49 +0800

    Finished most of game
    Today I had an afternoon session with mr wolf, and was able to get my
    solve puzzle function and teleport function to work.

--- puzzle.py
+++ puzzle.py
@@ -9,18 +9,38 @@ from quest.helpers import resolve_resource_path
 from quest.strategy import RandomWalk
 from quest.contrib.sprite_directionality import DirectionalMixin
 from quest.sprite import QuestSprite
+from game import PirateDialogue
+
+class Puzzle(InventoryItemMixin,NPC):
+    description = "coin"
+    def __init__(self,gamestate,image,scale):
+        self.gamestate=gamestate
+        super().__init__(image,scale)
 
 
+    def check(self,inventory):
+        if len(inventory) == 4:
+            npc_data = [
+                [Portal, "portal.png", 0.1, 400, 400]
+            ]
+            for sprite_class, image, scale, x, y in npc_data:
+                sprite = sprite_class(self.gamestate,image,scale)
+                sprite.center_x = x
+                sprite.center_y = y
+                self.gamestate.npc_list.append(sprite)
 
-class PuzzleKey():
-    puzzle_1=0
-
-class PuzzleImage():
-    puzzle_1=0
+    def on_collision(self, sprite, game):
+        super().on_collision(sprite,game)
+        self.check(self.gamestate.inventory())
 
 
-class Puzzlepiece(InventoryItemMixin,NPC):
-    description="item"
+class Portal(InventoryItemMixin,NPC):
 
-class Puzzle1(Puzzlepiece):
-    description="puzzle piece 1"
+    def __init__(self,gamestate,image,scale):
+        self.gamestate=gamestate
+        super().__init__(image,scale)
+
+    def on_collision(self,sprite,game):
+        talk_to_pirate()
+        #sprite.center_x = 100
+        #sprite.center_y = 100
