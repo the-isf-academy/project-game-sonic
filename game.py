6903abd8c1da2936a574ec9eed64a8da073245d8
Jared <0010100@student.isf.edu.hk>
2020-05-29 18:57:27 +0800

    Adding final touches to game

--- game.py
+++ game.py
@@ -33,30 +33,50 @@ class IslandAdventure(InventoryMixin,GrandmasSoupGame):
     player_sprite_image_down="boy_simple.png"
     player_sprite_image_up="boy_copy.png"
     player_scaling=0.7
-    screen_width = 1200
+    screen_width = 1000
     screen_height = 750
     left_viewport_margin = 96
     right_viewport_margin = 96
     bottom_viewport_margin = 96
     top_viewport_margin = 96
-    player_initial_x = 300
-    player_initial_y = 300
-    player_speed = 6
+    player_initial_x = 5*32
+    player_initial_y = (100-50)*32
+    player_speed = 10
 
     def __init__(self):
         super().__init__()
+        self.dialogue = Dialogue.from_ink("dialogue.ink")
+        self.modal = DialogueModal(self, self.dialogue)
+        self.dialogue1 = Dialogue.from_ink("dialogue1.ink")
+        self.modal1= DialogueModal(self, self.dialogue1)
+        self.dialogue2 = Dialogue.from_ink("dialogue2.ink")
+        self.modal2= DialogueModal(self, self.dialogue2)
+        self.dialogue3 = Dialogue.from_ink("dialogue3.ink")
+        self.modal3= DialogueModal(self, self.dialogue3)
+        self.InitTalk()
 
     def talk_with_pirate(self):
         self.open_modal(self.modal)
+
+    def Talk2(self):
+        self.open_modal(self.modal2)
+
+    def Escape(self):
+        self.open_modal(self.modal3)
 
     def setup_npcs(self):
         """Creates and places Grandma and the vegetables.
         """
         npc_data = [
-            [Puzzle, "carrots.png", 1, 240, 670],
-            [Puzzle, "mushroom.png", 1, 220, 645],
-            [Puzzle, "potatoes.png", 1, 250, 600],
-            [Puzzle, "tomatos.png", 1, 220, 580],
+            [Puzzle, "key.png", 1, 9*32, (99-31)*32],
+            [Puzzle, "key1.png", 1, 9*32, (99-31)*32],
+            [Puzzle, "key2.png", 1, 9*32, (99-31)*32],
+            [Puzzle, "key3.png", 1, 9*32, (99-31)*32],
+            [Puzzle2, "carrots.png", 1, 65*32, (99-15)*32],
+            [Puzzle2, "mushroom.png", 1, 136*32, (100-10)*32],
+            [Puzzle2, "potatoes.png", 1, 128*32, (100-72)*32],
+            [Puzzle2, "tomatos.png", 1, 45*32, (100-92)*32],
+            [Puzzle2, "tomatos.png", 1, 135*32, (100-92)*32],
         ]
         self.npc_list = arcade.SpriteList()
         for sprite_class, image, scale, x, y in npc_data:
@@ -78,18 +98,23 @@ class IslandAdventure(InventoryMixin,GrandmasSoupGame):
         """
         self.maps=[]
         sprite_classes = {
-            "Obstacles": Wall,
             "Background": QuestSprite,
+            "Obstacles": Wall,
         }
         self.add_map(TiledMap("island/combinedmaps.tmx",sprite_classes))
 
+    def InitTalk(self):
+        self.open_modal(self.modal1)
 
 
 class PlayerDirectional(DirectionalMixin,QuestSprite):
     pass
-
-
-
+"""
+            [Puzzle, "key.png", 1, 9*32, (99-31)*32],
+            [Puzzle, "key1.png", 1, 34*32, (100-34)*32],
+            [Puzzle, "key2.png", 1, 12*32, (100-60)*32],
+            [Puzzle, "key3.png", 1, 35*32, (100-61)*32],
+"""
 if __name__ == '__main__':
     game = IslandAdventure()
     game.run()
